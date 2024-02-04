1ed0d1d6101c776a54ab2e8464fbac3462a97e9e
Robert David Hernandez <rherna70@uic.edu>
2024-02-04 21:16:14 +0000

    init a rust project with a devcontainer and a basic hello world with a unit test

---
+++ app/hello.rs
@@ -0,0 +1,16 @@
+// app/hello.rs
+
+// suppress warning as there is no implementation yet,
+// This code just demonstrates how to create a module and a function
+// with a unit test
+#[allow(dead_code)]
+pub(crate) fn hello() {
+    println!("Hello, World!");
+}
+
+// suppress warning as there is no implementation yet,
+// This code just demonstrates how to create a module and a function
+// with a unit test
+#[allow(dead_code)]
+fn main() {
+}
