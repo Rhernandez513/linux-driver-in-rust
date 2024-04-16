5e384aa9c0b22baf92564cd0df0a1cff03a9ce0e
Riccardo Strina <riccardo.strina@icloud.com>
2024-04-16 01:03:16 -0500

    Add working rust driver for hw9 plus added ioctl.rs file to make everything work

--- devices/riccardo/lkp_enc_driver.rs
+++ devices/riccardo/lkp_enc_driver.rs
@@ -6,6 +6,8 @@ use core::result::Result::Ok;
 use core::ffi::c_void;
 use kernel::sync::Mutex;
 use kernel::bindings;
+use kernel::user_ptr::UserSlicePtr;
+// , _IOC_SIZESHIFT, _IOC_SIZEMASK
 use kernel::{
     ioctl::{_IOR, _IOW}, prelude::*, Module
 };
@@ -20,7 +22,7 @@ module! {
     license: "GPL",
 }
 
-const LKP_ENC_WRITE_SEED: u32 = _IOW::<u64>('q' as u32, 1);
+const LKP_ENC_WRITE_SEED: u32 = _IOW::<u32>('q' as u32, 1);
 const LKP_ENC_WRITE_STRING: u32 = _IOW::<&str>('q' as u32, 2);
 const LKP_ENC_READ_STRING: u32 = _IOR::<&str>('q' as u32, 3);
 
@@ -68,18 +70,51 @@ impl file::Operations for LkpEnc {
     fn ioctl(_data: <Self::Data as kernel::ForeignOwnable>::Borrowed<'_>,_file: &file::File,cmd: &mut file::IoctlCommand,) -> Result<i32> {
         pr_info!("qemu file ioctl\n");
         pr_info!("devmem {:?}", *DEVMEM.lock());
-        
-        match cmd.raw().0 {
+        let io_number = cmd.raw().0;
+        // let size = (io_number >> _IOC_SIZESHIFT) & _IOC_SIZEMASK;
+        let arg = cmd.raw().1;
+        let user_slice = unsafe { UserSlicePtr::new(arg as _, 255 as _)};
+
+        // pr_info!("User slice {:?}", user_slice.read_all());
+
+        match io_number {
             LKP_ENC_WRITE_SEED => {
-                let seed = cmd.raw().1;
+                let seed = user_slice.read_all().unwrap()[0] as u32;
                 pr_info!("LKP_ENC_WRITE_SEED : {:?}", seed);
+                unsafe {bindings::iowrite32(seed, *DEVMEM.lock() as _);}
             },
             LKP_ENC_READ_STRING => {
-                pr_info!("LKP_ENC_READ_STRING: {}", "Hello, World!");
+                // pr_info!("LKP_ENC_READ_STRING: {}", unsafe { bindings::ioread32(*DEVMEM.lock() as _) });
+                let mut ret: Vec<u8> = Vec::new();
+                for i in 0..255 {
+                    let c: u8 = unsafe { bindings::ioread8((*DEVMEM.lock() + 4 + i) as _) as u8 };
+                    let _ = ret.try_push(c);
+
+                    if c == 0 {
+                        break;
+                    }              
+                }
+
+                pr_info!("LKP_ENC_READ_STRING: {:?}", ret);
+
+                let _ = user_slice.write_all(&ret);
             },
             LKP_ENC_WRITE_STRING => {
-                let string = cmd.raw().1;
-                pr_info!("LKP_ENC_WRITE_STRING: {:?}", string);
+                let string = user_slice.read_all();
+
+                match string {
+                    Ok(s) => {
+                        for (i, c) in s.as_slice().iter().enumerate() {
+                            pr_info!("index: {:?}, char: {:?}", i, c);
+                            unsafe { bindings::iowrite8(*c, (*DEVMEM.lock() + 4 + i) as _) };
+                        }
+                    },
+                    Err(e) => {
+                        pr_info!("Error reading string: {:?}", e);
+                    }
+                }
+                // pr_info!("LKP_ENC_WRITE_STRING: {:?}", string);
+                
             },
             _ => {
                 pr_info!("Unknown IOCTL command");
@@ -106,8 +141,14 @@ impl Module for LkpEnc {
             // also 8-byte aligned because we checked it above.
             Ok(addr as usize)
         }?;
+        
+        // Test the memory mapping and writing to device
+        // pr_info!("devmem: {:?}", *ptr);
 
-        pr_info!("devmem: {:?}", *ptr);
+        // unsafe {
+        //     bindings::iowrite32(12, *ptr as _);
+        //     pr_info!("devmem: {:?}", bindings::ioread32(*ptr as _));
+        // }
 
         let mut chardev_reg = chrdev::Registration::new_pinned(name, 0, module)?;
         chardev_reg.as_mut().register::<LkpEnc>()?;
