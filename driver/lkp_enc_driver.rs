293ca241bd267387162143cb6e80e516ddacaf13
Riccardo Strina <riccardo.strina@icloud.com>
2024-04-18 19:51:24 -0500

    Code refactor for drive.rs

--- driver/lkp_enc_driver.rs
+++ driver/lkp_enc_driver.rs
@@ -1,18 +1,18 @@
 //! Rust HW9 driver module
-//! 
+//!
 //! This module is a Rust implementation of the HW9 driver module written in C.
 
 use core::result::Result::Ok;
-use core::ffi::c_void;
-use kernel::sync::Mutex;
 use kernel::bindings;
-use kernel::user_ptr::UserSlicePtr;
-// , _IOC_SIZESHIFT, _IOC_SIZEMASK
-use kernel::{
-    ioctl::{_IOR, _IOW}, prelude::*, Module
-};
-use kernel::file;
 use kernel::chrdev;
+use kernel::file;
+use kernel::sync::Mutex;
+use kernel::user_ptr::UserSlicePtr;
+use kernel::{
+    ioctl::{_IOR, _IOW},
+    prelude::*,
+    Module,
+};
 
 module! {
     type: LkpEnc,
@@ -26,40 +26,93 @@ const LKP_ENC_WRITE_SEED: u32 = _IOW::<u32>('q' as u32, 1);
 const LKP_ENC_WRITE_STRING: u32 = _IOW::<&str>('q' as u32, 2);
 const LKP_ENC_READ_STRING: u32 = _IOR::<&str>('q' as u32, 3);
 
-static DEVMEM: Mutex<usize> = unsafe {
-    Mutex::new(0)
-};
-
-#[allow(dead_code)]
 const DEVICE_BASE_PHYS_ADDR: u64 = 0xfebd1000;
+const SIZE: u64 = 4096;
+
+static DEVMEM: Mutex<usize> = unsafe { Mutex::new(0) };
 
 struct LkpEnc {
     _dev: Pin<Box<chrdev::Registration<1>>>,
+    // consider to switch to miscdev to remove DEVMEM and having it as a field in the struct
+    // for more info look at the examples in the rust samples directory
     // _dev: Pin<Box<miscdev::Registration<LkpEnc>>>,
+}
+
+fn read_device(ptr: usize) -> Vec<u8> {
+    let mut ret: Vec<u8> = Vec::new();
+    let bound = SIZE as usize;
+
+    for i in 0..bound {
+        let c: u8 = unsafe { bindings::ioread8((ptr + 4 + i) as _) as u8 };
+        let _ = ret.try_push(c);
+
+        if c == b'\0' {
+            break;
+        }
+    }
+
+    ret
+}
+
+fn write_device(ptr: usize, s: &[u8]) {
+    for (i, c) in s.iter().enumerate() {
+        unsafe { bindings::iowrite8(*c, (ptr + 4 + i) as _) };
+
+        if i == (SIZE - 5) as usize || *c == b'\0' {
+            break;
+        }
+    }
 }
 
 #[vtable]
 impl file::Operations for LkpEnc {
-    fn open(_context: &Self::OpenData,_file: &file::File) -> Result<Self::Data> {
+    fn open(_context: &Self::OpenData, _file: &file::File) -> Result<Self::Data> {
         pr_info!("qemu file opened\n");
         Ok(())
     }
 
-    fn write(_data: Self::Data,_file: &file::File,reader: &mut impl kernel::io_buffer::IoBufferReader,_offset:u64,) -> Result<usize> {
+    fn write(
+        _data: Self::Data,
+        _file: &file::File,
+        reader: &mut impl kernel::io_buffer::IoBufferReader,
+        _offset: u64,
+    ) -> Result<usize> {
         pr_info!("qemu file written\n");
-        Ok(reader.len())
+
+        match reader.read_all() {
+            Ok(mut s) => {
+                // null terminate the string before writing it to the device
+                // echo doesn't provide a null terminated string
+                let _ = s.try_push(b'\0');
+                write_device(*DEVMEM.lock(), &s);
+
+                // echo will expect a different number of bytes written, adding 0 to the end
+                // is not expected and will return an error
+                Ok(s.len() - 1)
+            }
+            Err(e) => {
+                pr_info!("Error reading string: {:?}", e);
+
+                Ok(0)
+            }
+        }
     }
 
-    fn read(_data: Self::Data,_file: &file::File,writer: &mut impl kernel::io_buffer::IoBufferWriter,offset:u64,) -> Result<usize> {
+    fn read(
+        _data: Self::Data,
+        _file: &file::File,
+        writer: &mut impl kernel::io_buffer::IoBufferWriter,
+        offset: u64,
+    ) -> Result<usize> {
         pr_info!("qemu file read\n");
-    
-        // Message to write only once
-        let message = b"Hello, World!\n";
+
         // If the offset is 0, it means we're starting to read from the beginning.
         // If the offset is greater than 0, in this simple case, we assume the message was already read,
         // and thus we return Ok(0) to indicate no more data is to be read.
         if offset == 0 {
-            let _ = writer.write_slice(message);
+            let message = read_device(*DEVMEM.lock());
+            let _ = writer.write_slice(&message);
+
             Ok(message.len())
         } else {
             // No more data to read, indicate this by returning 0 bytes read.
@@ -67,60 +120,57 @@ impl file::Operations for LkpEnc {
         }
     }
 
-    fn ioctl(_data: <Self::Data as kernel::ForeignOwnable>::Borrowed<'_>,_file: &file::File,cmd: &mut file::IoctlCommand,) -> Result<i32> {
+    fn ioctl(
+        _data: <Self::Data as kernel::ForeignOwnable>::Borrowed<'_>,
+        _file: &file::File,
+        cmd: &mut file::IoctlCommand,
+    ) -> Result<i32> {
         pr_info!("qemu file ioctl\n");
-        pr_info!("devmem {:?}", *DEVMEM.lock());
         let io_number = cmd.raw().0;
-        // let size = (io_number >> _IOC_SIZESHIFT) & _IOC_SIZEMASK;
-        let arg = cmd.raw().1;
-        let user_slice = unsafe { UserSlicePtr::new(arg as _, 255 as _)};
-
-        // pr_info!("User slice {:?}", user_slice.read_all());
+        let ptr = cmd.raw().1;
+        let user_slice = unsafe { UserSlicePtr::new(ptr as _, SIZE as _) };
 
         match io_number {
             LKP_ENC_WRITE_SEED => {
-                let seed = user_slice.read_all().unwrap()[0] as u32;
-                pr_info!("LKP_ENC_WRITE_SEED : {:?}", seed);
-                unsafe {bindings::iowrite32(seed, *DEVMEM.lock() as _);}
-            },
-            LKP_ENC_READ_STRING => {
-                // pr_info!("LKP_ENC_READ_STRING: {}", unsafe { bindings::ioread32(*DEVMEM.lock() as _) });
-                let mut ret: Vec<u8> = Vec::new();
-                for i in 0..255 {
-                    let c: u8 = unsafe { bindings::ioread8((*DEVMEM.lock() + 4 + i) as _) as u8 };
-                    let _ = ret.try_push(c);
+                let seed: u32 = match user_slice.read_all() {
+                    Ok(s) => {
+                        if s.len() >= 1 {
+                            s[0] as u32
+                        } else {
+                            0
+                        }
+                    }
+                    Err(e) => {
+                        pr_info!("Error reading seed: {:?}", e);
 
-                    if c == 0 {
-                        break;
-                    }              
+                        0
+                    }
+                };
+
+                unsafe {
+                    bindings::iowrite32(seed, *DEVMEM.lock() as _);
                 }
-
-                pr_info!("LKP_ENC_READ_STRING: {:?}", ret);
+            }
+            LKP_ENC_READ_STRING => {
+                let ret = read_device(*DEVMEM.lock());
 
                 let _ = user_slice.write_all(&ret);
-            },
-            LKP_ENC_WRITE_STRING => {
-                let string = user_slice.read_all();
-
-                match string {
-                    Ok(s) => {
-                        for (i, c) in s.as_slice().iter().enumerate() {
-                            pr_info!("index: {:?}, char: {:?}", i, c);
-                            unsafe { bindings::iowrite8(*c, (*DEVMEM.lock() + 4 + i) as _) };
-                        }
-                    },
-                    Err(e) => {
-                        pr_info!("Error reading string: {:?}", e);
-                    }
+            }
+            LKP_ENC_WRITE_STRING => match user_slice.read_all() {
+                Ok(s) => {
+                    write_device(*DEVMEM.lock(), &s);
                 }
-                // pr_info!("LKP_ENC_WRITE_STRING: {:?}", string);
-                
+                Err(e) => {
+                    pr_info!("Error reading string: {:?}", e);
+                    return Ok(-1);
+                }
             },
             _ => {
                 pr_info!("Unknown IOCTL command");
+                return Ok(-1);
             }
         }
-        
+
         Ok(0)
     }
 }
@@ -128,9 +178,9 @@ impl file::Operations for LkpEnc {
 impl Module for LkpEnc {
     fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
         pr_info!("{} module init", name);
-        
-        let addr = unsafe { bindings::ioremap(DEVICE_BASE_PHYS_ADDR, 4096) };
-        
+
+        let addr = unsafe { bindings::ioremap(DEVICE_BASE_PHYS_ADDR, SIZE) };
+
         // devmem know contains the address mapped in the CPU of the device
 
         let mut ptr = DEVMEM.lock();
@@ -141,14 +191,6 @@ impl Module for LkpEnc {
             // also 8-byte aligned because we checked it above.
             Ok(addr as usize)
         }?;
-        
-        // Test the memory mapping and writing to device
-        // pr_info!("devmem: {:?}", *ptr);
-
-        // unsafe {
-        //     bindings::iowrite32(12, *ptr as _);
-        //     pr_info!("devmem: {:?}", bindings::ioread32(*ptr as _));
-        // }
 
         let mut chardev_reg = chrdev::Registration::new_pinned(name, 0, module)?;
         chardev_reg.as_mut().register::<LkpEnc>()?;
@@ -164,6 +206,6 @@ impl Module for LkpEnc {
 impl Drop for LkpEnc {
     fn drop(&mut self) {
         pr_info!("lkp_enc module exit\n");
-        unsafe { bindings::iounmap(*DEVMEM.lock() as *mut c_void ) };
+        unsafe { bindings::iounmap(*DEVMEM.lock() as _) };
     }
 }
