3fafbf3109e3cb8c09f2eb0ad4c15e42d6f5a4d5
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2024-04-22 18:30:06 -0500

    Update setup-vm.sh

--- scripts/setup-vm.sh
+++ scripts/setup-vm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mknod /dev/lkp_enc_driver c 250 0
-ls -l /dev/lkp_enc_driver
+# For use inside the rust-enabled kernel running the rust driver
+
+mknod /dev/lkp_enc c 250 0
+ls -l /dev/lkp_enc
 
 apt install build-essential zsh
